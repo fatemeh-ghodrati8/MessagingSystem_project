d03f04619c39de37c827f2dd02e62a3050c7181e
fatemeh-ghodrati8 <Fatemehghodrati820@gmail.com>
2026-07-15 16:30:28 +0430

    Add attachment upload support

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from uuid import uuid4
 from flask import (
     Flask, abort, flash, redirect, render_template,
     request, send_from_directory, session, url_for)
+    
 from sqlalchemy import or_
 from werkzeug.utils import secure_filename
 
